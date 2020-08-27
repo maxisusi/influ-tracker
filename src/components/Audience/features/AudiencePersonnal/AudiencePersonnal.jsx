6e1856560c848fd2cc91457d89c14973fe881d54
maxisusi <maxbastienbalej@gmail.com>
2020-08-27 13:28:52 +0200

    Ethnicity chart added

--- src/components/Audience/features/AudiencePersonnal/AudiencePersonnal.jsx
+++ src/components/Audience/features/AudiencePersonnal/AudiencePersonnal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import styles from './AudiencePersonnal.module.css';
 import { Bar } from 'react-chartjs-2';
 
@@ -6,13 +6,13 @@ const AudiencePersonnal = ({ audiencePersonnal }) => {
 
     const mData = audiencePersonnal.audienceStatistics.audienceGenderAge.male;
     const fData = audiencePersonnal.audienceStatistics.audienceGenderAge.female;
-
     const { male, female } = audiencePersonnal.audienceStatistics.audienceGender;
-
     const maleData = Object.values(mData);
     const femaleData = Object.values(fData);
 
-    const data = {
+    const [menu, setMenu] = useState('ageGender');
+
+    const ageGenderData = {
         labels: ['0-17', '18-24', '25-34', '35-44', '45-54', '55-64'],
         datasets: [
             {
@@ -32,13 +32,59 @@ const AudiencePersonnal = ({ audiencePersonnal }) => {
 
         ]
     }
-    const [menu, setMenu] = useState('ageGender');
+
+    const audienceRace = audiencePersonnal.audienceStatistics.audienceRace;
+    const audienceRaceTitle = Object.keys(audienceRace)
+    const audienceRaceValues = Object.values(audienceRace);
+
+
+    const ethnicityData = {
+
+        labels: audienceRaceTitle.reverse(),
+        datasets: [
+            {
+                label: 'Bitch',
+                data: audienceRaceValues.reverse(),
+                borderWidth: 1.6,
+                borderColor: '#00D5FC',
+                backgroundColor: '#00D5FC99',
+            },
+
+        ]
+
+    }
+
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case 'AGE_GENDER': {
+                return { data: ageGenderData }
+            }
+
+            case 'AGE_OTHER': {
+                return { data: ethnicityData }
+            }
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, ageGenderData);
+
+    useEffect(() => {
+        if (menu === 'ageGender') {
+            dispatch({ type: 'AGE_GENDER' });
+        }
+        if (menu === 'ethnicity') {
+            dispatch({ type: 'AGE_OTHER' });
+        }
+    }, [menu, audiencePersonnal])
+
+
     return (
         <div className={styles.audiencePersonnalWrapper}>
             <div className={styles.chartMenu}>
                 <p onClick={() => setMenu('ageGender')} style={{ color: menu === 'ageGender' ? '#00CFFC' : '#B0B6BB' }}>Age & Gender</p>
-                {/* <p onClick={() => setMenu('ethnicity')} style={{ color: menu === 'ethnicity' ? '#00CFFC' : '#B0B6BB' }}>Ethnicity</p>
-                <p onClick={() => setMenu('language')} style={{ color: menu === 'language' ? '#00CFFC' : '#B0B6BB' }}>Language</p> */}
+                <p onClick={() => setMenu('ethnicity')} style={{ color: menu === 'ethnicity' ? '#00CFFC' : '#B0B6BB' }}>Ethnicity</p>
+                {/* <p onClick={() => setMenu('language')} style={{ color: menu === 'language' ? '#00CFFC' : '#B0B6BB' }}>Language</p> */}
             </div>
 
             <div className={styles.subMenuInfo}>
@@ -55,7 +101,7 @@ const AudiencePersonnal = ({ audiencePersonnal }) => {
             </div>
 
             <Bar
-                data={data}
+                data={state.data}
                 width={100}
                 height={10}
                 options={{
@@ -65,7 +111,7 @@ const AudiencePersonnal = ({ audiencePersonnal }) => {
                         bodyFontSize: 15,
                         bodySpacing: 2,
                         callbacks: {
-                            label: function(tooltipItems, data) { 
+                            label: function (tooltipItems, data) {
                                 return tooltipItems.yLabel + '%';
                             }
                         }
@@ -80,7 +126,7 @@ const AudiencePersonnal = ({ audiencePersonnal }) => {
                             fontSize: 16
                         },
                         callbacks: {
-                            label: function(tooltipItems, data) { 
+                            label: function (tooltipItems, data) {
                                 return tooltipItems.yLabel + '%';
                             }
                         }
